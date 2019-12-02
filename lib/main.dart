96877d445e7a8bdd95bc7fe85719e7ef79bae4dd
Olaxan <fredrik.lind.96@gmail.com>
2019-12-02 17:07:02 +0100

    Ready-up bg_geo on main.

--- lib/main.dart
+++ lib/main.dart
@@ -2,17 +2,32 @@ import 'package:flutter/material.dart';
 import 'package:orientx/simon_directory/first_screen.dart';
 import "simon_directory/login_page.dart";
 
+import 'package:flutter_background_geolocation/flutter_background_geolocation.dart'
+    as bg;
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    bg.BackgroundGeolocation.ready(bg.Config(
+            desiredAccuracy: bg.Config.DESIRED_ACCURACY_HIGH,
+            distanceFilter: 10.0,
+            stopOnTerminate: false,
+            startOnBoot: true,
+            debug: true,
+            logLevel: bg.Config.LOG_LEVEL_WARNING))
+        .then((bg.State state) {
+      if (!state.enabled) {
+        bg.BackgroundGeolocation.start();
+      }
+    });
+
     return MaterialApp(
         title: 'OrientX',
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: LoginPage()
-    );
+        home: LoginPage());
   }
 }
