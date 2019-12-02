57af41e68b597a4aba907bc91be081670998f5a3
Olaxan <fredrik.lind.96@gmail.com>
2019-12-02 16:54:54 +0100

    Fixed merge error in main.

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:orientx/simon_directory/first_screen.dart';
 import "simon_directory/login_page.dart";
-import 'package:flutter_background_geolocation/flutter_background_geolocation.dart' as bg;
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-      title: 'OrientX',
-      home: LoginPage()
-        bg.BackgroundGeolocation.start();
-        body: MapView(),
+    return MaterialApp(
+        title: 'OrientX',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: LoginPage()
+    );
+  }
+}
